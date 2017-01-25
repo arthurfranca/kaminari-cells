f7c56b18d17c14b2dce159a6d6bd82cb53f40c52
arthurfranca <arthur.a.franca@gmail.com>
2017-01-25 15:39:55 -0200

    Add compatibility to kaminari 1.0

--- lib/kaminari-cells.rb
+++ lib/kaminari-cells.rb
@@ -6,7 +6,7 @@ require "cell/partial"
 module Kaminari
   module Helpers
     module CellsHelper
-      include Kaminari::ActionViewExtension
+      include Kaminari::Helpers::HelperMethods
       include ActionView::Helpers::OutputSafetyHelper
       include ActionView::Helpers::TranslationHelper
       include Cell::ViewModel::Partial
@@ -14,6 +14,12 @@ module Kaminari
       def paginate(scope, options = {}, &block)
         options = options.reverse_merge(:views_prefix => "../views/")
         super
+      end
+    end
+
+    class Paginator < Tag
+      def render(&block)
+        instance_eval(&block) if @options[:total_pages] > 1
       end
     end
   end
